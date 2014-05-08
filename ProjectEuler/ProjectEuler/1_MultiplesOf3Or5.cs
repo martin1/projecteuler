01762f8f1bf97c45b9a77fbf28036c39b7e24845
Martin Kurgi <martinkurgi@gmail.com>
2014-05-08 21:39:20 +0300

    Added solutions for problems 1 and 2

---
+++ ProjectEuler/ProjectEuler/1_MultiplesOf3Or5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+    class MultiplesOf3Or5
+    {
+        //Uncomment to run
+        //static void Main(string[] args)
+        //{
+        //    int sum = 0;
+            
+        //    foreach (var number in getNumbers(1000))
+        //    {
+        //        sum += number;
+        //    }
+
+        //    Console.WriteLine("sum: {0}", sum);
+        //}
+
+        public static IEnumerable<int> getNumbers(int upperLimit)
+        {
+            for (int i = 0; i < upperLimit; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
